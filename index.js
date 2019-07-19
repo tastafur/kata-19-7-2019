dcc5834817207c30c17baf90378f8a228ceede4f
tastafur <ruben.romero@guidesmiths.com>
2019-07-19 16:14:26 +0200

    add three test

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 exports.lockAndSay = (number) => {
-    return `1${number}` 
+    const digits = (''+number).split('')
+    let countDigit = 1 
+    digits.forEach((digit, i) =>{
+        if(i > 0){
+            if(digit === digits[i -1])
+                countDigit++
+        }
+    })
+    return `${countDigit}${digits[0]}` 
 }
--- index.spec.js
+++ index.spec.js
@@ -8,4 +8,8 @@ describe('Look-and-say sequence', () => {
     it('should return three concat with the digit when the input one digit', () => {
         expect('13').to.be.equal(lockAndSay(3))
     })
+
+    it('should return number of times the digit is repeated concat with the digit when the input is more than one the digit and all the digits all the same', () => {
+        expect('51').to.be.equal(lockAndSay(11111))
+    })
 })
