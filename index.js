315cb86b6277d64a2f607f0a9f4bb53203ac6c5d
tastafur <ruben.romero@guidesmiths.com>
2019-07-19 17:25:14 +0200

    change word in name method

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-exports.lockAndSay = (number) => {
+exports.lookAndSay = (number) => {
     const digits = (''+number).split('')
-    let countDigit = 1 
+    let countDigit = 1
+    let result = ''
+    if(digits.length === 1)
+        return `1${number}`
+        
     digits.forEach((digit, i) =>{
         if(i > 0){
-            if(digit === digits[i -1])
+            if(digit === digits[i -1]){
                 countDigit++
+                if(i + 1 === digits.length) {
+                    result = result + countDigit + digits[i -1]
+                }
+            } else {
+                result = result + countDigit + digits[i -1]
+                countDigit = 1
+            }
         }
     })
-    return `${countDigit}${digits[0]}` 
+    return result
 }
--- index.spec.js
+++ index.spec.js
@@ -1,15 +1,23 @@
 const expect = require('chai').expect
-const { lockAndSay } = require('./index')
+const { lookAndSay } = require('./index')
 describe('Look-and-say sequence', () => {
     it('should return one concat with the digit when the input one digit', () => {
-        expect('11').to.be.equal(lockAndSay(1))
+        expect('11').to.be.equal(lookAndSay(1))
     })
 
     it('should return three concat with the digit when the input one digit', () => {
-        expect('13').to.be.equal(lockAndSay(3))
+        expect('13').to.be.equal(lookAndSay(3))
     })
 
     it('should return number of times the digit is repeated concat with the digit when the input is more than one the digit and all the digits all the same', () => {
-        expect('51').to.be.equal(lockAndSay(11111))
+        expect('51').to.be.equal(lookAndSay(11111))
+    })
+
+    it('should return the number of times that a digit is repeated and this digit for each block of similar numbers when the input is a sequence of numbers', () => {
+        expect('111221').to.be.equal(lookAndSay(1211))
+    })
+
+    it('should return the number of times that a digit is repeated and this digit for each block of similar numbers when the input is a sequence of numbers for 312211', () => {
+        expect('13112221').to.be.equal(lookAndSay(312211))
     })
 })
